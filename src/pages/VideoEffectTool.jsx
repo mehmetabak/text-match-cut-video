0c0b21b21b61c68e15a9668caebe9b45353fce36
Mehmet Abak <70433403+mehmetabak@users.noreply.github.com>
2026-07-31 14:11:26 +0300

    feat: add VideoEffectTool page for processing ken-burns and vhs-tape video effects

--- src/pages/VideoEffectTool.jsx
+++ src/pages/VideoEffectTool.jsx
@@ -158,7 +158,7 @@ export default function VideoEffectTool() {
           </p>
         </header>
         
-        <main className="flex-1 flex flex-col min-h-0">
+        <main className="flex-1 flex flex-col min-h-0 pb-[calc(4rem+env(safe-area-inset-bottom))] lg:pb-0">
           <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 lg:gap-8 flex-1 min-h-0">
             
             {/* Left Column: Uploader & Progress */}
